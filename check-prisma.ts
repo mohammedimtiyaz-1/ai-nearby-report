fcf6244681656243357291981eadb4a9ca0b0953
Mohammed-imtiyaz <mohammedimtiyaz.jk@yahoo.com>
2026-05-19 15:53:11 +0530

    Cleanup: remove temporary check script and build info

--- check-prisma.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-console.log('Available models:', Object.keys(prisma).filter(k => !k.startsWith('_') && !k.startsWith('$')))
